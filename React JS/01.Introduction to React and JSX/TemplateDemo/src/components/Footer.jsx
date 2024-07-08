5381af0aab3c1fe7f46f0b6c0c1a701150b04153
Vanya Dimitrova <vapedim@yahoo.com>
2024-07-08 15:30:34 +0300

    Format Footer and Video Sections

--- React JS/01.Introduction to React and JSX/TemplateDemo/src/components/Footer.jsx
+++ React JS/01.Introduction to React and JSX/TemplateDemo/src/components/Footer.jsx
@@ -1,15 +1,15 @@
 function Footer() {
-  return (
-    <footer className="footer-copy">
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-12">
-          <p>2018 &copy; Applight. Website Designed by <a href="http://w3Template.com" target="_blank" rel="dofollow">W3 Template</a></p>
-        </div>
-      </div>
-    </div>
-  </footer>
-  );
+    return (
+        <footer className="footer-copy">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-12">
+                        <p>2018 &copy; Applight. Website Designed by <a href="http://w3Template.com" target="_blank" rel="dofollow">W3 Template</a></p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
