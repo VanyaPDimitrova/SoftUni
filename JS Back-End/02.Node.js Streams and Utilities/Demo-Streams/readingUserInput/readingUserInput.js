41346eab9dde6483b5dc62f5e4a5d109fafd6836
Vanya Dimitrova <vapedim@yahoo.com>
2024-07-11 17:37:28 +0300

    Reading user input

---
+++ JS Back-End/02.Node.js Streams and Utilities/Demo-Streams/readingUserInput/readingUserInput.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+// create stream that read from the terminal and write directly in a file
+// start file in terminal with command node ./readingUserInput/readingUserInput.js
+
+//1. // process.stdin - read from terminal and log in to terminal
+// process.stdin.on('data', (chunk) => {
+//     console.log('>>>', chunk.toString());
+// });
+
+//2. // read from terminal and write in file output.txt
+// const writer = fs.createWriteStream('./output.txt');
+// process.stdin.on('data', (chunk) => {
+//     writer.write(chunk.toString());
+// });
+
+//3. // pipe dos both from 2. - read from terminal and write in to file. But it delete file content. It do not writes in the end of file. 
+const writer = fs.createWriteStream('./output.txt');
+process.stdin.pipe(writer);
