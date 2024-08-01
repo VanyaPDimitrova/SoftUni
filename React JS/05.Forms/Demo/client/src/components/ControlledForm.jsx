516709fc9b20cdb23fa33355f8ab320bdab564a3
Vanya Dimitrova <vapedim@yahoo.com>
2024-08-01 15:05:34 +0300

    add more fields

--- React JS/05.Forms/Demo/client/src/components/ControlledForm.jsx
+++ React JS/05.Forms/Demo/client/src/components/ControlledForm.jsx
@@ -4,6 +4,11 @@ function ControlledForm() {
     const [formValues, setFormValues] = useState({
         username: '',
         password: '',
+        email: '',
+        age: '',
+        sex: 'm',
+        bio: '',
+        occupation: 'qa'
     });
 
     useEffect(() => {
@@ -49,6 +54,84 @@ function ControlledForm() {
                         value={formValues.password}
                         onChange={formValuesChangeHAndler}
                     />
+                </div>
+
+                <div>
+                    <label htmlFor="email">Email </label>
+                    <input 
+                        type="email" 
+                        name='email' 
+                        id='email'
+                        value={formValues.email}
+                        onChange={formValuesChangeHAndler}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="age">Age </label>
+                    <input 
+                        type="number" 
+                        name='age' 
+                        id='age'
+                        value={formValues.age}
+                        onChange={formValuesChangeHAndler}
+                    />
+                </div>
+
+                <div style={{ padding: '10px' }}>
+                    <span style={{ padding: '10px' }}>Sex: </span>
+                    <label htmlFor="sex-m">Male </label>
+                    <input 
+                        type="radio" 
+                        name='sex' 
+                        id='sex-m'
+                        value='m'
+                        onChange={formValuesChangeHAndler}
+                        checked={formValues.sex === 'm'}
+                    />
+                    <label htmlFor="sex-f">Female </label>
+                    <input 
+                        type="radio" 
+                        name='sex' 
+                        id='sex-f'
+                        value='f'
+                        onChange={formValuesChangeHAndler}
+                        checked={formValues.sex === 'f'}
+
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="sports">Sports</label>
+                    <input 
+                        type="checkbox" 
+                        name=''
+                        id=''
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="bio">Bio</label>
+                    <textarea 
+                        name="bio" 
+                        id="bio" 
+                        value={formValues.bio}
+                        onChange={formValuesChangeHAndler}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="occupation">Occupation </label>
+                    <select 
+                        name="occupation" 
+                        id="occupation"
+                        value={formValues.occupation}
+                        onChange={formValuesChangeHAndler}
+                    >
+                        <option value="dev">Dev</option>
+                        <option value="ba">Ba</option>
+                        <option value="qa">QA</option>
+                    </select>
                 </div>
 
                 <input type="submit" value="Login" />
