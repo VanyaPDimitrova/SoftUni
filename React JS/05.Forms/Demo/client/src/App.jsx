c136c331139abe062c754aaeeeedff6e4d0d561a
Vanya Dimitrova <vapedim@yahoo.com>
2024-08-01 12:28:05 +0300

    - add server - move project in client folder

--- React JS/05.Forms/Demo/client/src/App.jsx
+++ React JS/05.Forms/Demo/client/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import UncontrolledForm from './components/UncontrolledForm';
+import ControlledForm from './components/ControlledForm';
 
 function App() {
 
   return (
     <>
       <UncontrolledForm />
+      <ControlledForm />
     </>
   );
 }
