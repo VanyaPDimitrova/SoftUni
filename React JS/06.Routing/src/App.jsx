bd8a9bcd6fd9e4bab14ac223699ddf1766bfd142
Vanya Dimitrova <vapedim@yahoo.com>
2024-08-02 14:17:35 +0300

    create Routes

--- React JS/06.Routing/src/App.jsx
+++ React JS/06.Routing/src/App.jsx
@@ -1,11 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './components/Home';
+import About from './components/About';
+import Pricing from './components/Pricing';
 
 function App() {
 
     return (
-        <>
-            <Home />
-        </>
+        <div className="bg-white">
+            <Header />
+
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/pricing' element={<Pricing />} />
+            </Routes>
+            
+        </div>
     );
 }
 
