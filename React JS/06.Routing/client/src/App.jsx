0687a4fa6d0abae70ec90cb7489cbfb4a209aaeb
Vanya Dimitrova <vapedim@yahoo.com>
2024-08-06 19:36:47 +0300

    remove /* from parent of nested routs

--- React JS/06.Routing/client/src/App.jsx
+++ React JS/06.Routing/client/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/about/*' element={<About />}>
+                <Route path='/about' element={<About />}>
                     <Route path='mission' element={<Mission /> } />
                     <Route path='contacts' element={<Contacts /> } />
                     <Route path='team' element={<OurTeam /> } />
