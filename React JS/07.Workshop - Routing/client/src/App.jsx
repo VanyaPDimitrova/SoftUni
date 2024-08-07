0c3c01b21f91cbd4bdbcec4f12259da107aa3849
Vanya Dimitrova <vapedim@yahoo.com>
2024-08-07 23:51:21 +0300

    add components

--- React JS/07.Workshop - Routing/client/src/App.jsx
+++ React JS/07.Workshop - Routing/client/src/App.jsx
@@ -1,8 +1,16 @@
+import Header from './header/Header';
+import Home from './home/Home';
 
 function App() {
   return (
-    <>
-    </>
+    <div id="box">
+        <Header />
+
+        <main id="main-content">
+            <Home />
+            
+        </main>
+    </div>
   );
 }
 
