543668d789eaffc53ded47a50f57df658b686256
Vanya Dimitrova <vapedim@yahoo.com>
2024-08-09 15:13:18 +0300

    at Home: - add background-image - add categories section

--- React JS/Final Project - Quiz System/client/src/components/home/Home.jsx
+++ React JS/Final Project - Quiz System/client/src/components/home/Home.jsx
@@ -1,10 +1,15 @@
-import styles from './Main.module.css';
+import styles from './Home.module.css';
 function Home() {
+
     return (
         <section className={styles.home}> 
             <div className={styles.container}>
-                <h1>Hello from Home</h1>
-
+                <h1>Let's do QUIZ</h1>
+                
+                <div className={styles.categories}>
+                    <h2>All quiz topics</h2>    
+                    
+                </div>
             </div>
         </section>
     );
