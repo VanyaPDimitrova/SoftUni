c9b8bc2b46c913041f6b0d62523a66d9fd0b6141
Vanya Dimitrova <vapedim@yahoo.com>
2024-08-09 15:14:20 +0300

    add CategoriesRequests

---
+++ React JS/Final Project - Quiz System/client/src/components/requests/CategoriesRequests.js
@@ -0,0 +1,40 @@
+import MAIN_URL from '../common/MainUrl';
+
+export const getAllCategories = () => {
+  return fetch(`${MAIN_URL}/categories`)
+    .then((res) => res.json());
+};
+
+export const createCategory = (data) => {
+  return fetch(`${MAIN_URL}/categories`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token()}`,
+    },
+    body: JSON.stringify(data)
+  })
+    .then((res) => res.json());
+};
+
+export const updateCategory = (data, categoryId) => {
+  return fetch(`${MAIN_URL}/categories/${categoryId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token()}`,
+    },
+    body: JSON.stringify(data)
+  })
+    .then((res) => res.json());
+};
+
+export const deleteCategoryById = (categoryId) => {
+  return fetch(`${MAIN_URL}/categories/${categoryId}`, {
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${token()}`,
+    },
+  })
+    .then((res) => res.json());
+};
