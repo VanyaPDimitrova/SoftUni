dcdb4d6bf7384c36a03f95c7a523356abf18444f
Vanya Dimitrova <vapedim@yahoo.com>
2024-08-09 11:12:53 +0300

    remove Main component reorganize components

--- React JS/Final Project - Quiz System/client/src/App.jsx
+++ React JS/Final Project - Quiz System/client/src/App.jsx
@@ -1,19 +1,25 @@
-import { BrowserRouter } from 'react-router-dom';
-import Footer from './components/footer/Footer';
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/header/Header';
-import Main from './components/main/Main';
+import Home from './components/home/Home';
+import Contacts from './components/contacts/Contacts';
+import Footer from './components/footer/Footer';
+
 
 function App() {
 
-  return (
-    <BrowserRouter>
-      <Header />
-
-      <Main />
-
-      <Footer />
-    </BrowserRouter>
-  );
+    return (
+        <>
+            <Header />
+                <main>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/Contacts' element={<Contacts />} />
+                    </Routes>
+                </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
--- React JS/Final Project - Quiz System/client/src/main.jsx
+++ React JS/Final Project - Quiz System/client/src/main.jsx
@@ -1,10 +1,15 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
 import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </StrictMode>
 );
