3db5441c9bb351746e26ceea0b27932eb740cf5d
Vanya Dimitrova <vapedim@yahoo.com>
2024-08-14 23:46:51 +0300

    add useFetch in auth-requester.js

--- React JS/Final Project - Quiz System/client/src/components/requests/auth-requester.js
+++ React JS/Final Project - Quiz System/client/src/components/requests/auth-requester.js
@@ -1,23 +1,20 @@
 const AUTH_URL = 'http://localhost:3030/users'
 
+const useFetch = (page,  data ) => {
+    return fetch(`${AUTH_URL}${page}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ...data })
+      })
+        .then((res) => res.json());
+}
+
 export const login = ({ email, password }) => {
-    return fetch(`${AUTH_URL}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password })
-    })
-      .then((res) => res.json());
+    return useFetch('/login', { email, password });
 };
 
   export const register = ({ email, password, role }) => {
-    return fetch(`${AUTH_URL}/register`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password, role })
-    })
-      .then((res) => res.json());
+    return useFetch('/register', { email, password, role }); 
   };
