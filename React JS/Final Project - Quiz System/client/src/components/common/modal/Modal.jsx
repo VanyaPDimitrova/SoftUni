5a11a92da5efacee4b2386731aa25ca0e243c511
Vanya Dimitrova <vapedim@yahoo.com>
2024-08-14 14:46:02 +0300

    refactor modal

--- React JS/Final Project - Quiz System/client/src/components/common/modal/Modal.jsx
+++ React JS/Final Project - Quiz System/client/src/components/common/modal/Modal.jsx
@@ -1,26 +1,22 @@
 import styles from './Modal.module.css';
 
-function Modal({ open, close, header, content }) {
+function Modal({ close, header, content }) {
     return(
-        <>
-            {open && 
-                <div className={styles.overlay} onClick={close}>
-                    <div className={styles.modal} onClick={e => e.stopPropagation()} >
-                        <div className={styles.container}>
-                            <header className={styles.headers}>
-                                <h2>{header}</h2>
-                        
-                                <button className={styles.btnClose} onClick={close}>                            
-                                    <i className="fa-solid fa-square-xmark"></i>                        
-                                </button>
-                            </header>
-    
-                            {content}
-                        </div>
+        <div className={styles.overlay} onClick={close}>
+            <div className={styles.modal} onClick={e => e.stopPropagation()} >
+                <div className={styles.container}>
+                    <header className={styles.headers}>
+                        {header}
+                
+                        <button className={styles.btnClose} onClick={close}>                            
+                            <i className="fa-solid fa-square-xmark"></i>                        
+                        </button>
+                    </header>
+
+                    {content}
                 </div>
             </div>
-            }
-        </>
+        </div>
     );
 }
 export default Modal;
